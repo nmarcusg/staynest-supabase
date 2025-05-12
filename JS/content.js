e012c59be9f37072d7aaaae14bbb806d615653ab
nmarcusg <89849200+nmarcusg@users.noreply.github.com>
2025-05-13 04:37:20 +0800

    added js

--- JS/content.js
+++ JS/content.js
@@ -1,12 +1,6 @@
 const supabaseUrl = "https://wkbljryfnphbthnfbghj.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrYmxqcnlmbnBoYnRobmZiZ2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2MzQyODEsImV4cCI6MjA2MTIxMDI4MX0.W8Tcg8whyMUqS0yvOenEaNU6wrFzLr1vWNRhJ6rNOac";
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey, {
-    auth: {
-      persistSession: true,       // Store session in localStorage
-      autoRefreshToken: true,     // Keep session tokens fresh
-      detectSessionInUrl: true,   // Read URL for OAuth redirects, if any
-    }
-  });
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
   
 
 
