005c83616f6e2f751757827bbd701190f0501c38
Razi <122347913+RAZI23@users.noreply.github.com>
2025-05-19 23:38:39 +0800

    Added initNav

--- JS/host.js
+++ JS/host.js
@@ -1,8 +1,11 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { initNav } from './navbar.js';
 
 const supabaseUrl = "https://wkbljryfnphbthnfbghj.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrYmxqcnlmbnBoYnRobmZiZ2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2MzQyODEsImV4cCI6MjA2MTIxMDI4MX0.W8Tcg8whyMUqS0yvOenEaNU6wrFzLr1vWNRhJ6rNOac";
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+initNav(supabase); 
 
 const citySelect = document.getElementById('property-city-select');
 const cityInput = document.getElementById('property-city-input');
